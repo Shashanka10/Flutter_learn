d564f3b35adb5241608c41409bfb1042c5fe3b38
Shashanka <71580178+Shashanka10@users.noreply.github.com>
2023-08-25 22:13:40 +0545

    learned listing

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:info_popup/info_popup.dart";
 
 class LearnFlutterPage extends StatefulWidget {
   const LearnFlutterPage({super.key});
@@ -9,10 +10,28 @@ class LearnFlutterPage extends StatefulWidget {
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
   bool isSwitch = false;
+  bool? isCheck = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(12.0),
+            child: InfoPopupWidget(
+              contentMaxWidth: 250,
+              contentTheme: InfoPopupContentTheme(
+                infoTextStyle: TextStyle(color: Colors.green),
+                infoContainerBackgroundColor:
+                    Color.fromARGB(255, 255, 255, 255),
+                contentPadding: EdgeInsets.all(20),
+              ),
+              contentTitle:
+                  'Information on the widgets that can be used in flutter',
+              child: Icon(Icons.info_outline_rounded),
+            ),
+          ),
+        ],
         title: const Text("Learn Flutter"),
         automaticallyImplyLeading: false,
         leading: IconButton(
@@ -22,77 +41,91 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      body: Column(
-        children: [
-          Image.asset("images/flutter_einstein.png"),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          Container(
-            color: const Color.fromARGB(255, 50, 94, 95),
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(10),
-            width: double.infinity,
-            child: const Center(
-              child: Text(
-                "This is a text widget",
-                style: TextStyle(color: Colors.white),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("images/flutter_einstein.png"),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              color: const Color.fromARGB(255, 50, 94, 95),
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  "This is a text widget",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              debugPrint("Elevated buton clicked");
-            },
-            child: const Text("Elevated Button"),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint("Outlined buton clicked");
-            },
-            child: const Text("Outlined Button"),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint("Textbutton buton clicked");
-            },
-            child: const Text("Textbutton Button"),
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              debugPrint("This is the row");
-            },
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(
-                  Icons.local_airport,
-                  color: Color.fromARGB(255, 21, 97, 159),
-                ),
-                Text(
-                  "Row Widget",
-                  style: TextStyle(color: Color.fromARGB(255, 81, 10, 93)),
-                ),
-                Icon(
-                  Icons.fire_truck,
-                  color: Color.fromARGB(255, 141, 30, 22),
-                ),
-              ],
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwitch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () {
+                debugPrint("Elevated buton clicked");
+              },
+              child: const Text("Elevated Button"),
             ),
-          ),
-          Switch(
-            value: isSwitch,
-            onChanged: (bool newbool) {
-              setState(() {
-                isSwitch = newbool;
-              });
-            },
-          ),
-        ],
+            OutlinedButton(
+              onPressed: () {
+                debugPrint("Outlined buton clicked");
+              },
+              child: const Text("Outlined Button"),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint("Textbutton buton clicked");
+              },
+              child: const Text("Textbutton Button"),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint("This is the row");
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.local_airport,
+                    color: Color.fromARGB(255, 21, 97, 159),
+                  ),
+                  Text(
+                    "Row Widget",
+                    style: TextStyle(color: Color.fromARGB(255, 81, 10, 93)),
+                  ),
+                  Icon(
+                    Icons.fire_truck,
+                    color: Color.fromARGB(255, 141, 30, 22),
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+              value: isSwitch,
+              onChanged: (bool newbool) {
+                setState(() {
+                  isSwitch = newbool;
+                });
+              },
+            ),
+            Checkbox(
+                value: isCheck,
+                onChanged: (bool? newbool) {
+                  setState(() {
+                    isCheck = newbool;
+                  });
+                }),
+            Image.network(
+                "https://miro.medium.com/v2/resize:fit:828/format:webp/1*oiJQQh8fv7HeRgKjLYIG4A.png")
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_learn/homepage.dart';
+import 'package:flutter_app_learn/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,13 +30,17 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int currentPage = 0;
+  List<Widget> pages = const [
+    HomePage(),
+    ProfilePage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("App Learn"),
       ),
-      body: const HomePage(),
+      body: pages[currentPage],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint("floating Action Button");
