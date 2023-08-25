8429a10c5add56e2dcd461ea1eadf0713381cbd8
Shashanka <71580178+Shashanka10@users.noreply.github.com>
2023-08-25 21:16:59 +0545

    added row and used switch too

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -8,6 +8,7 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +25,73 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
       body: Column(
         children: [
           Image.asset("images/flutter_einstein.png"),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+            color: const Color.fromARGB(255, 50, 94, 95),
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                "This is a text widget",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint("Elevated buton clicked");
+            },
+            child: const Text("Elevated Button"),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              debugPrint("Outlined buton clicked");
+            },
+            child: const Text("Outlined Button"),
+          ),
+          TextButton(
+            onPressed: () {
+              debugPrint("Textbutton buton clicked");
+            },
+            child: const Text("Textbutton Button"),
+          ),
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              debugPrint("This is the row");
+            },
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  Icons.local_airport,
+                  color: Color.fromARGB(255, 21, 97, 159),
+                ),
+                Text(
+                  "Row Widget",
+                  style: TextStyle(color: Color.fromARGB(255, 81, 10, 93)),
+                ),
+                Icon(
+                  Icons.fire_truck,
+                  color: Color.fromARGB(255, 141, 30, 22),
+                ),
+              ],
+            ),
+          ),
+          Switch(
+            value: isSwitch,
+            onChanged: (bool newbool) {
+              setState(() {
+                isSwitch = newbool;
+              });
+            },
+          ),
         ],
       ),
     );
